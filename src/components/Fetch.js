0e004d4f87870f91c6b07b497c4b3479d1a434b3
usman <malikusman5959@gmail.com>
2022-03-15 12:58:07 +0500

    Done

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export const fetchFeeds = async (orderData)  => {
     try {
           let res = await axios.get(`https://dwinnex.com/feed/`);
-       
         return res;
     } catch (error) {
         console.log(error);
